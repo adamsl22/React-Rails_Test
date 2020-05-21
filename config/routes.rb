f1ec32a7f863a5c004aeacd649d2a4d4ab221da7
github username <aslongman22@gmail.com>
2020-05-21 14:15:53 -0400

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'pages/index'
-  get 'videos', to: 'pages#episodes'
+  get 'videos', to: 'pages#videos'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
