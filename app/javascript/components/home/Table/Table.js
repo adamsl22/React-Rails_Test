dce6363375bb846ee9a1c80c166304953474cb27
github username <aslongman22@gmail.com>
2020-05-21 12:52:09 -0400

    table component

---
+++ app/javascript/components/home/Table/Table.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Table extends React.Component{
+    render(){
+        return(
+            <div className='pt-5 pb-5'>
+                <div className='container'>
+                    <div className='text-center'>
+                        <h2 className='pt-4 pb-4'>Videos</h2>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
