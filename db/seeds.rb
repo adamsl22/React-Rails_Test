c8fa3939cab28944184d902e26e06ad6ef11cbd0
github username <aslongman22@gmail.com>
2020-05-21 13:59:01 -0400

    backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Video.create(title: 'Video 1', description: 'Video 1 Description', url: 'Video 1 url')
+Video.create(title: 'Video 2', description: 'Video 2 Description', url: 'Video 2 url')
+Video.create(title: 'Video 3', description: 'Video 3 Description', url: 'Video 3 url')
+Video.create(title: 'Video 4', description: 'Video 4 Description', url: 'Video 4 url')
